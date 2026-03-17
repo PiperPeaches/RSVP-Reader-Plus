5216f82fb49ca7d025d413369aaedf5068b2f026
PiperPeaches <Itoasted319@gmail.com>
2026-03-16 22:14:06 -0400

    init

---
+++ Services/ArticleService.cs
@@ -0,0 +1,21 @@
+using SmartReader;
+
+namespace rsvpreader.Services;
+
+public class ArticleService
+{
+    public async Task<(string Title, string[] Words)> GetArticleAsync(string url)
+    {
+        var Reader = new Reader(url);
+        var artical = await Reader.GetArticleAsync();
+
+        if(artical.IsReadable)
+        {
+            var words = artical.TextContent
+                .Split(new[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return (artical.Title, words);
+        }
+        return ("Error", new[] { "Could", "not", "extract", "content."});
+    }
+}
